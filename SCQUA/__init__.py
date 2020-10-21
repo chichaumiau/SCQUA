edc4b94a3aa4c3af778a73036b60a880b8b87038
Zhichao (Chichau) Miao <chichaumiau@gmail.com>
2020-10-21 09:52:58 +0100

    add readquant.py

--- SCQUA/__init__.py
+++ SCQUA/__init__.py
@@ -315,6 +315,158 @@ import sys
 import struct
 import gzip
 
+import sys,os
+from glob import iglob
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+
+def read_kallisto(sample_path):
+    ''' Function for reading a Kallisto quantification result.
+    Returns
+    -------
+    A pandas.Series with the expression values in the sample.
+    '''
+    quant_file = sample_path + '/abundance.tsv'
+    df = pd.read_table(quant_file, engine='c',
+                                   usecols=['target_id', 'tpm','est_counts'],
+                                   index_col=0,
+                                   dtype={'target_id': np.str, 'tpm': np.float64, 'est_counts':np.float64 })
+
+    df = df.rename(columns={'tpm': 'TPM', 'est_counts':'counts','target_id':'Name'})
+    return df
+
+
+
+def read_salmon(sample_path, tp ='gene'):
+    ''' Function for reading a Salmon quantification result.
+
+    Parameters
+    ----------
+    isoforms : bool, default False
+        Whether to parse isoform level expression or gene level expression.
+
+    Returns
+    -------
+    A pandas.Series with the expression values in the sample.
+    '''
+    read_kwargs = {
+        'engine': 'c',
+        'usecols': ['Name', 'TPM','NumReads'],
+        'index_col': 0,
+        'dtype': {'Name': np.str, 'TPM': np.float64, 'NumReads': np.float64}
+    }
+    if tp == 'gene':
+        df = pd.read_table('%s/quant.genes.sf'%sample_path, **read_kwargs)
+    else:
+        df = pd.read_table('%s/quant.sf'%sample_path, **read_kwargs)
+    df = df.rename(columns={'NumReads': 'counts'})
+    return df
+
+def read_salmon_qc(sample_path, flen_lim=(100, 100)):
+    ''' Parse technical quality control data from a Salmon quantification
+    result.
+
+    Parameters
+    ----------
+    flen_lim, tuple (int start, int end), default (100, 100)
+        How many bases to remove from start and end of fragment length
+        distribution when calculating the robust mode. This is too see if
+        things roughly worked out even if the max FLD Salmon parameter was
+        set too small.
+
+    Returns
+    -------
+    A pandas.Series with technical information from the Salmon results for
+    the sample.
+    '''
+    flen_dist = np.fromfile('%s/libParams/flenDist.txt'%sample_path, sep='\t')
+    global_fl_mode = flen_dist.argmax()
+    robust_fl_mode = flen_dist[flen_lim[0]:-flen_lim[1]].argmax() + flen_lim[0]
+
+    qc_data = pd.read_json('%s/aux_info/meta_info.json'%sample_path, typ='series')  
+    qc_data = qc_data[['num_processed', 'num_mapped', 'percent_mapped']]
+    qc_data['global_fl_mode'] = global_fl_mode
+    qc_data['robust_fl_mode'] = robust_fl_mode
+
+    return qc_data
+
+def read_quants(pattern='salmon/*_salmon_out', tool='salmon', **kwargs):
+    ''' Read quantification results from every directory matching the glob
+    in pattern.
+    Parameters
+    ----------
+    tool, str, default 'salmon'
+        The quantification tool used to generate the results. Currently
+        supports 'salmon', 'sailfish', 'kallisto', and 'cufflinks'.
+    **kwargs,
+        kwargs are passed on to the tool specific sample parser. See documentation
+        for individual parsers for details.
+    Returns
+    -------
+    A pandas.DataFrame where columns are samples, rows are genes, and cells
+    contain the expression value.
+    '''
+    sample_readers = {
+        'salmon': read_salmon,
+	'kallisto':read_kallisto
+    }
+
+    quant_reader = sample_readers[tool]
+
+    tpm_quants = pd.DataFrame()
+    cts_quants = pd.DataFrame()
+    for sample_path in tqdm(iglob(pattern)):
+        if os.path.isdir(sample_path):
+            #print sample_path
+            if not os.path.isfile('%s/quant.genes.sf'%sample_path):
+                print("Not found %s"%sample_path)
+                #os.system('rm %s -rf'%sample_path)
+                continue
+            sample_quant = quant_reader(sample_path, **kwargs)
+            
+            id=sample_path.split('/')[-1]
+            tpm_quants[id] = sample_quant['TPM']
+            cts_quants[id] = sample_quant['counts']
+
+    return tpm_quants,cts_quants
+
+def read_qcs(pattern='salmon/*_salmon_out', tool='salmon', **kwargs):
+    ''' Read technical quality control data results from every directory
+    matching the glob in pattern.
+    Parameters
+    ----------
+    tool, str, default 'salmon'
+        The quantification tool used to generate the results. Currently
+        supports 'salmon' and 'sailfish'.
+    **kwargs,
+        kwargs are passed on to the tool specific sample parser. See documentation
+        for individual parsers for details.
+    Returns
+    -------
+    A pandas.DataFrame where rows are samples, and columns are technical
+    features extrated from the tool results.
+    '''
+    sample_readers = {
+        'salmon': read_salmon_qc,
+    }
+
+    qc_reader = sample_readers[tool]
+
+    QCs = pd.DataFrame()
+    for sample_path in tqdm(iglob(pattern)):
+        if os.path.isdir(sample_path):
+            if not os.path.isfile('%s/quant.genes.sf'%sample_path):
+                #os.system('rm %s -rf'%sample_path)
+                print('Not found: %s'%sample_path)
+                continue
+            sample_qc = qc_reader(sample_path, **kwargs)
+            id=sample_path.split('/')[-1]
+            QCs[id] = sample_qc
+
+    return QCs.T
+
 class PosModel:
 	def __init__(self):
 		self.models = {}
